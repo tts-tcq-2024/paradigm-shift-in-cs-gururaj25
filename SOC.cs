5bb6f944938d5648c41c3a9273c94062fbb1b20f
gururaj25 <51817915+gururaj25@users.noreply.github.com>
2024-07-24 19:57:14 +0530

    Create SOC.cs

---
+++ SOC.cs
@@ -0,0 +1,22 @@
+public class SOC : IChecker
+{
+    public bool Check(float soc)
+    {
+        if (soc < 20 || soc > 80)
+        {
+            Console.WriteLine("State of Charge is out of range!");
+            return false;
+        }
+        return true;
+
+    }
+
+    public void DisplayStatus(float soc)
+    {
+        Console.WriteLine("State of Charge is: " + soc);
+    }
+    public void Warn()
+    {
+        Console.WriteLine("Warn");
+    }
+}
