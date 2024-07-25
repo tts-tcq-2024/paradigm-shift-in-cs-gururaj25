95fd0fdf3778fa85bbc4919dfd2b3fdba03a497b
gururaj25 <51817915+gururaj25@users.noreply.github.com>
2024-07-25 08:55:33 +0530

    Update BatteryCheckerTests.cs

--- BatteryCheckerTests.cs
+++ BatteryCheckerTests.cs
@@ -1,3 +1,15 @@
 public class BatteryCheckerTests
 {
+  static void ExpectTrue(bool expression) {
+        if(!expression) {
+            Console.WriteLine("Expected true, but got false");
+            Environment.Exit(1);
+        }
+    }
+    static void ExpectFalse(bool expression) {
+        if(expression) {
+            Console.WriteLine("Expected false, but got true");
+            Environment.Exit(1);
+        }
+    }
 }
