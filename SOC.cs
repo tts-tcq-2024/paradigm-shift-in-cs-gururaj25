7689f792bfe74f7d75f43ce4de5f7a9785e1d101
gururaj25 <51817915+gururaj25@users.noreply.github.com>
2024-07-31 12:04:55 +0530

    Update SOC.cs

--- SOC.cs
+++ SOC.cs
@@ -2,26 +2,45 @@ using System;
 using System.Diagnostics;
 namespace paradigm_shift_csharp
 {
-public class SOC : IChecker
-{
-    public bool Check(float soc)
+    public class SOC : IChecker
     {
-        if (soc < 20 || soc > 80)
+        private const int MinSOC = 20;
+        private const int MaxSOC = 80;
+
+        public bool Check(float soc)
         {
-            Console.WriteLine("State of Charge is out of range!");
-            return false;
+            if (soc < MinSOC || soc > MaxSOC)
+            {
+                
+                return false;
+            }
+            return true;
         }
-        return true;
 
+        public void DisplayStatus(float soc)
+        {
+            
+            if (soc < MinSOC)
+            {
+                Console.WriteLine("State of Charge is below the minimum threshold!");
+            }
+            else if (soc > MaxSOC)
+            {
+                Console.WriteLine("State of Charge is above the maximum threshold!");
+            }
+            else
+            {
+                Console.WriteLine("State of Charge is within the acceptable range.");
+            }
+        }
+
+        public void Warn()
+        {
+            Console.WriteLine("Warn");
+        }
     }
 
-    public void DisplayStatus(float soc)
-    {
-        Console.WriteLine("State of Charge is: " + soc);
-    }
-    public void Warn()
-    {
-        Console.WriteLine("Warn");
-    }
-}
+    
+
+   
 }
