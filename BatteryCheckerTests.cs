6c57b756149040447aaafdbbd13c8095b251d785
gururaj25 <51817915+gururaj25@users.noreply.github.com>
2024-07-31 12:00:27 +0530

    Update BatteryCheckerTests.cs

--- BatteryCheckerTests.cs
+++ BatteryCheckerTests.cs
@@ -1,15 +1,24 @@
-public class BatteryCheckerTests
+using System;
+
+namespace paradigm_shift_csharp
 {
-  static void ExpectTrue(bool expression) {
-        if(!expression) {
-            Console.WriteLine("Expected true, but got false");
-            Environment.Exit(1);
+    public class BatteryCheckerTests
+    {
+        public  static void ExpectTrue(bool expression)
+        {
+            if (!expression)
+            {
+                Console.WriteLine("Expected true, but got false");
+                Environment.Exit(1);
+            }
         }
-    }
-    static void ExpectFalse(bool expression) {
-        if(expression) {
-            Console.WriteLine("Expected false, but got true");
-            Environment.Exit(1);
+        public  static void ExpectFalse(bool expression)
+        {
+            if (expression)
+            {
+                Console.WriteLine("Expected false, but got true");
+                Environment.Exit(1);
+            }
         }
     }
 }
