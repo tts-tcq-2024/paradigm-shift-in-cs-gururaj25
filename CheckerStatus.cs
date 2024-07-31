fbe6da44a65047d86aaef2f962549c0d7310e5e4
gururaj25 <51817915+gururaj25@users.noreply.github.com>
2024-07-31 19:55:17 +0530

    Create CheckerStatus.cs

---
+++ CheckerStatus.cs
@@ -0,0 +1,12 @@
+namespace paradigm_shift_csharp
+{
+    public enum CHECKERSTATUS
+    {
+        LOW_BREACH,
+        LOW_WARNING,
+        NORMAL,
+        HIGH_WARNING,
+        HIGH_BREACH
+    }
+    
+}
