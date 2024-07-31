75a0fa05c23c6f74e4923f4438294224a382aabb
gururaj25 <51817915+gururaj25@users.noreply.github.com>
2024-07-31 12:01:42 +0530

    Update IChecker.cs

--- IChecker.cs
+++ IChecker.cs
@@ -2,10 +2,10 @@ using System;
 using System.Diagnostics;
 namespace paradigm_shift_csharp
 {
-  public interface IChecker
-  {
-      bool Check(float value);
-      void DisplayStatus(float value);
-      void Warn();
-  }
+    public interface IChecker
+    {
+        bool Check(float value);
+        void DisplayStatus(float value);
+        void Warn();
+    }
 }
