5a9c8d0210fb89f784ff2ae419c01cd22aed71fb
gururaj25 <51817915+gururaj25@users.noreply.github.com>
2024-07-31 14:41:30 +0530

    Update BatteryChecker.cs

--- BatteryChecker.cs
+++ BatteryChecker.cs
@@ -18,10 +18,7 @@ namespace paradigm_shift_csharp
             var tempResult = checkTemp.Check(GetValueForCheck(checkTemp, temperature, soc, chargeRate));
             var socResult = checksoc.Check(GetValueForCheck(checksoc, temperature, soc, chargeRate));
             var chargeRateResult = checkChargeRate.Check(GetValueForCheck(checkChargeRate, temperature, soc, chargeRate));
-                 bool returnresult = tempResult && socResult && chargeRateResult;
-                Console.WriteLine($"TempResult: {tempResult} SOCResult :{socResult} ChargeResult :{chargeRateResult} ReturnResult:{returnresult}");
-            return returnresult; 
-          
+                 return tempResult && socResult && chargeRateResult;                          
         }
 
         private float GetValueForCheck(IChecker check, float temperature, float soc, float chargeRate)
