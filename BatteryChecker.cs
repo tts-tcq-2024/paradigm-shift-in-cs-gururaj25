27b351ca2aea7bb3789516c031c85b9207f02ea3
gururaj25 <51817915+gururaj25@users.noreply.github.com>
2024-07-31 11:59:49 +0530

    Update BatteryChecker.cs

--- BatteryChecker.cs
+++ BatteryChecker.cs
@@ -2,34 +2,35 @@ using System;
 using System.Diagnostics;
 namespace paradigm_shift_csharp
 {
-   
-  public class BatteryChecker
-{
-    private  IChecker check;
-     
-    public BatteryChecker(IChecker check)
-    {
-     this.check =check;
-    }
-    public bool IsBatteryOk(float temperature, float soc, float chargeRate)
-    {
-      IChecker  checkTemp =  new Temperature();
-      IChecker  checksoc   =new SOC();
-      IChecker  checkChargeRate  =new ChargeRate() ;
-      var tempResult= checkTemp.Check(GetValueForCheck(checkTemp, temperature, soc, chargeRate);
-      var socResult= checksoc.Check(GetValueForCheck(checksoc, temperature, soc, chargeRate);
-      var chargeRateResult= checkChargeRate.Check(GetValueForCheck(checkChargeRate, temperature, soc, chargeRate)
-      
-      return tempResult && socResult && chargeRateResult;
-       
-    }
 
-    private float GetValueForCheck(IChecker check, float temperature, float soc, float chargeRate)
+    public class BatteryChecker
     {
-        if (check is TemperatureCheck) return temperature;
-        if (check is SocCheck) return soc;
-        if (check is ChargeRateCheck) return chargeRate;
-        
+        private IChecker check;
+
+        public BatteryChecker(IChecker check)
+        {
+            this.check = check;
+        }
+        public bool IsBatteryOk(float temperature, float soc, float chargeRate)
+        {
+            IChecker checkTemp = new Temperature();
+            IChecker checksoc = new SOC();
+            IChecker checkChargeRate = new ChargeRate();
+            var tempResult = checkTemp.Check(GetValueForCheck(checkTemp, temperature, soc, chargeRate));
+            var socResult = checksoc.Check(GetValueForCheck(checksoc, temperature, soc, chargeRate));
+            var chargeRateResult = checkChargeRate.Check(GetValueForCheck(checkChargeRate, temperature, soc, chargeRate));
+
+                    return tempResult && socResult && chargeRateResult;
+
+        }
+
+        private float GetValueForCheck(IChecker check, float temperature, float soc, float chargeRate)
+        {
+            float returnCheck =0;
+            if (check is Temperature) returnCheck= temperature ;
+            if (check is SOC) returnCheck= soc;
+            if (check is ChargeRate) returnCheck = chargeRate;
+            return returnCheck;
+        }
     }
-  }
 }
